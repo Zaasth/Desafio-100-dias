37b45a2cd0bdc5fe8e5f127543dcbb3033a5350b
Zaasth <wellintonpaypal@gmail.com>
2024-05-08 23:10:42 -0300

    Desafio: dia 011

---
+++ Dias/$011.java
@@ -0,0 +1,26 @@
+package edu.wellinton.desafio.Dia1;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class $011 {
+    public static void main(String[] args) {
+        String time12Hour = "8:30 PM"; // Coloque aqui o horário PM que você deseja converter
+
+        try {
+
+            DateFormat inputFormat = new SimpleDateFormat("hh:mm a");
+
+            DateFormat outputFormat = new SimpleDateFormat("HH:mm");
+
+            Date date = inputFormat.parse(time12Hour);
+
+            String time24Hour = outputFormat.format(date);
+
+            System.out.println("Horário em formato de 24 horas: " + time24Hour);
+        } catch (ParseException e) {
+            System.out.println("Erro ao fazer a conversão: " + e.getMessage());
+        }
+    }
+}
